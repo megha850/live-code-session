64c7297fd2a35d832ce00f4547cc4b55be640aa3
megha850 <124565299+megha850@users.noreply.github.com>
2023-06-16 17:41:25 +0000

    user login and logout

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,4 +1,5 @@
 // signup a new user
+import { json } from "express";
 import user from "../models/user.schema.js"
 
 import asyncHandler from "../service/asyncHandler";
@@ -8,6 +9,13 @@ export const cookieOptions = {
     expires: new Date(Date.now() + 3 * 24 * 60 * 60 * 1000),
     httpOnly: true
 }
+
+/***********************************************
+ * @SIGNUP
+ * @route http://localhost:5000/api/auth/signup
+ * @description User signUp controller for creating new user
+ * @returns User Object 
+ ************************************************/
 
 export const signup = asyncHandler(async(req, res) => {
     // get data from user
@@ -41,5 +49,44 @@ export const signup = asyncHandler(async(req, res) => {
         success: true,
         token, 
         user,
+    })
+})
+
+export const login = asyncHandler(async (req, res) => {
+   const {email, password} = req.body
+
+// validation
+if (!email || !password) {
+    throw new CustomError("Please fill the field", 400)
+}
+const User = user.findOne({email}).select("+password")
+
+if (!User) {
+    throw new CustomError("Invalid Credentials", 400)
+}
+
+  const ispasswordMatched = await User.comparePassword(password)
+
+  if (ispasswordMatched) {
+    const token = user.getJWTtoken()
+    User.password = undefined
+    res.cookie("token", token, cookieOptions)
+    return res.status(200).json({
+        success: true,
+        token,
+        user
+    })
+  }
+  throw new CustomError("password is incorrect", 400)
+})
+
+export const logout = asyncHandler(async (req, res) => {
+    res.cookie("token", null, {
+        expires: new Date(Date.now()),
+        httpOnly: true
+    })
+    res.status(200).json({
+        success: true,
+        message: "Logged out"
     })
 })
