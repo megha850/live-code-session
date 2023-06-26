0c0b8407d8d7f1bada30dc090e671bf72d958c36
megha850 <124565299+megha850@users.noreply.github.com>
2023-06-26 15:08:47 +0530

    coupon controller is done

--- src/controllers/coupon.controller.js
+++ src/controllers/coupon.controller.js
@@ -27,6 +27,49 @@ export const createCoupon = asyncHandler(async (req, res) => {
     })
 })
 
+export const updateCoupon = asyncHandler(async (req, res) => {
+    const {id: couponId} = req.params
+    const {action} = req.body
+
+    // action is boolean or not
+
+    const coupon = await Coupon.findByIdandUpdate(
+        couponId,
+        {
+            active: action
+        },
+        {
+            new: true,
+            runValidators: true
+        }
+    )
+    if (!coupon) {
+    throw new CustomError("coupon not found", 404)
+    }
+
+    res.status(200).json({
+        success: true,
+        message: "Coupon updated",
+        coupon
+    })
+})
+
+export const deleteCoupon = asyncHandler(async(req, res) => {
+    const {id: couponId} = req.params
+
+    const coupon = await Coupon.findByIdandUpdate(couponId)
+    if (!coupon) {
+    throw new CustomError("coupon not found", 404)
+    }
+
+    res.status(200).json({
+        success: true,
+        message: "Coupon updated",
+        coupon
+    })
+
+})
+
 export const getAllCoupon = asyncHandler(async (req, res) => {
     const allCoupons = await Coupon.find();
 
